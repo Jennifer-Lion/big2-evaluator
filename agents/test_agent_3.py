63980e0185c5ee054e5e169df1ce51f75012c207
Jennifer-Lion <41191958+Jennifer-Lion@users.noreply.github.com>
2023-02-27 08:00:40 +0800

    Add test agents

---
+++ agents/test_agent_3.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  8 15:57:58 2019
+
+"""
+
+from logic import Logic
+from card import Card
+
+logic = Logic()
+card = Card()
+
+def action(hand, field, control, turn, field_history, enemy1, enemy2, enemy3):
+    
+    if type(hand) != list:
+        hand = hand.translate({ord('"'):None})
+        hand = hand.translate({ord(']'):None})
+        hand = hand.translate({ord('['):None})
+        hand = hand.translate({ord("'"):None})
+        hand = hand.translate({ord(' '):None})
+        hand = hand.split(",")
+
+    if type(field) != list:
+        if field == "[',']":
+            field = []
+        else:
+            field = field.translate({ord('"'):None})
+            field = field.translate({ord(']'):None})
+            field = field.translate({ord('['):None})
+            field = field.translate({ord("'"):None})
+            field = field.translate({ord(' '):None})
+            field = field.split(",")
+
+    if control == "true" or control == "True":
+        control = True
+    elif control == "false" or control == "False":
+        control = False
+    
+    moveLists = logic.possibleMoves(hand, field, control, turn)
+    if len(moveLists) == 0:
+        return []
+    else:
+        return moveLists[0]
+
+import sys
+
+if __name__ == "__main__":
+    st1=sys.argv[1]
+    st2=sys.argv[2]
+    st3=sys.argv[3]
+    st4=int(sys.argv[4])
+    st5=sys.argv[5]
+    st6=int(sys.argv[6])
+    st7=int(sys.argv[7])
+    st8=int(sys.argv[8])
+    print(" ".join(action(st1, st2, st3, st4, st5, st6, st7, st8)))
